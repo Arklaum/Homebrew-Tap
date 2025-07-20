ba983800882fcd57fb9533c68b6dd96dfeec7c05
Jo <141064017+Arklaum@users.noreply.github.com>
2025-07-20 12:29:37 +0100

    Update youtube-music.rb

--- Casks/youtube-music.rb
+++ Casks/youtube-music.rb
@@ -1,22 +1,15 @@
-require 'json'
-require 'net/http'
-
+# youtube-music.rb
 cask "youtube-music" do
-  # This block helps create the correct filename suffix for arm64 vs intel
   arch arm: "-arm64"
 
-  # Define the specific version and checksums
-  # This should be updated when a new app version is released
   version "3.10.0"
-  sha256 arm:   "ee30ccf7be62be04e17d94d54f0eedcfc74ee4048cde15fd9c9540ece1cecaf2"
+  sha256 "ee30ccf7be62be04e17d94d54f0eedcfc74ee4048cde15fd9c9540ece1cecaf2"
 
+  url "https://github.com/th-ch/youtube-music/releases/download/v#{version}/YouTube-Music-#{version}#{arch}.dmg"
+  name "YouTube Music"
   desc "YouTube Music Desktop App"
   homepage "https://github.com/th-ch/youtube-music"
 
-  # The URL is now built cleanly from the static version and arch
-  url "https://github.com/th-ch/youtube-music/releases/download/v#{version}/YouTube-Music-#{version}#{arch}.dmg"
-
-  # This block now works correctly to check for new versions
   livecheck do
     url :url
     strategy :github_latest
